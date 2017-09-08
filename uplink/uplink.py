d5eb6fbf7fae9c6451dc377a7ce85da4d746c5f1
Grzegorz Gajoch <gajoch@gmail.com>
2017-09-08 23:36:47 +0100

    AFSK uplink gnuradio + python use

---
+++ uplink/uplink.py
@@ -0,0 +1,37 @@
+import afsk_uplink
+import thread
+import aprs
+import kiss
+import time
+
+
+class Uplink:
+    def __init__(self, host='localhost', port=1234):
+        self.kiss = kiss.TCPKISS(host, port)
+        self.frame = aprs.Frame()
+        self.frame.source = self.frame.destination = aprs.Callsign("PWSAT2-0");
+        self.frame.text = ""
+
+    def start(self):
+        thread.start_new_thread(afsk_uplink.main, ())
+        time.sleep(0.5)
+        self.kiss.start()
+
+    def stop(self):
+        pass
+
+    def send(self, payload):
+        self.frame.text = payload
+        self.kiss.write(self.frame.encode_kiss())
+
+
+if __name__ == "__main__":
+    up = Uplink()
+    up.start()
+    up.send("AAAAAAAAAAAAAAAA")
+    up.send("BBBBBBBBBBBBBBBB")
+    up.send("CCCCCCCCCCCCCCCC")
+
+    time.sleep(5)
+
+    up.stop()
