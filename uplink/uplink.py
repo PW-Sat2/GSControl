39eeeb132aef1d33e58f2ac72de9d0e114be7361
Grzegorz Gajoch <gajoch@gmail.com>
2017-09-14 11:49:18 +0200

    Uplink script tested

--- uplink/uplink.py
+++ uplink/uplink.py
@@ -1,9 +1,9 @@
 import afsk_uplink
 import thread
+from threading import Timer
 import aprs
 import kiss
 import time
-
 
 class Uplink:
     def __init__(self, host='localhost', port=8001):
@@ -12,13 +12,21 @@ class Uplink:
         self.frame.source = self.frame.destination = aprs.Callsign("PWSAT2-0");
         self.frame.text = ""
 
+    def start_tb(self):
+        self.tb = afsk_uplink.afsk_uplink()
+        self.tb.start()
+
     def start(self):
-        thread.start_new_thread(afsk_uplink.main, ())
+        thread.start_new_thread(self.start_tb, ())
+
         time.sleep(0.5)
+
         self.kiss.start()
 
     def stop(self):
-        pass
+        self.tb.stop()
+        self.kiss.stop()
+        self.tb.wait()
 
     def send(self, payload):
         self.frame.text = payload
