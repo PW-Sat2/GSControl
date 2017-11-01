ba7998035a69f780b46456e987a6879177777c84
Grzegorz Gajoch <gajoch@gmail.com>
2017-11-01 21:04:44 +0100

    WIP: requests for beacon added

--- bench/scripts/tests/sads_experiment.py
+++ bench/scripts/tests/sads_experiment.py
@@ -5,18 +5,24 @@ import tmtc
 from tm import TM, Check
 from tmtc import Tmtc
 from tc.experiments import AbortExperiment, PerformSADSExperiment
-
+from tc.comm import SendBeacon
 
 tmtc = Tmtc()
 checker = Check(tmtc)
 
 time.sleep(1)
 
-checker.check(TM.Experiments.CurrentExperimentCode, None, 0)
+tmtc.send(SendBeacon())
+time.sleep(5)
+checker.check(TM.Experiments.CurrentExperimentCode, 'None', 0)
 
 
 tmtc.send(PerformSADSExperiment())
-checker.check(TM.Experiments.CurrentExperimentCode, 6, 200)
+tmtc.send(SendBeacon())
+time.sleep(5)
+checker.check(TM.Experiments.CurrentExperimentCode, 'SADS', 300)
 
 tmtc.send(AbortExperiment())
-checker.check(TM.Experiments.CurrentExperimentCode, None, 200)
+tmtc.send(SendBeacon())
+time.sleep(5)
+checker.check(TM.Experiments.CurrentExperimentCode, 'None', 200)
