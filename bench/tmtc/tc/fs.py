ffa6b11dadfe45294829f271f575ece246fab08f
begreeen <michal.gumiela@gmail.com>
2017-11-01 23:05:01 +0100

    WIP: missing frames in list files

--- bench/tmtc/tc/fs.py
+++ bench/tmtc/tc/fs.py
@@ -8,7 +8,7 @@ class ListFiles(object):
         self.path = path
 
     def send(self, tmtc):
-    	tmtc.send_tc_with_multi_response(telecommand.fs.ListFiles, response_frames.file_system.FileListSuccessFrame, self.path)
+    	frames = tmtc.send_tc_with_multi_response(telecommand.fs.ListFiles, response_frames.file_system.FileListSuccessFrame, self.path)
     	files = []
         for f in frames:
             files.extend(RemoteFileTools.parse_file_list(f))
