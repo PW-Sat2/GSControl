9152bb5567c01a1f398b3e867f9a8064fdee49c6
begreeen <michal.gumiela@gmail.com>
2017-11-02 19:45:55 +0100

    test remove file

---
+++ bench/scripts/tests/FS/test_remove_file.py
@@ -0,0 +1,23 @@
+import sys, time, pprint
+sys.path.append('../../..')
+
+import tmtc
+from tm import TM, Check
+from tmtc import Tmtc
+from tc.fs import GetFileInfo, RemoveFile
+
+tmtc = Tmtc()
+checker = Check(tmtc)
+
+time.sleep(1)
+
+# Request info about particular file
+chosen_file = tmtc.send(GetFileInfo('/', 'telemetry.current'))
+pprint.pprint(chosen_file)
+
+# Remove file
+tmtc.send(RemoveFile('telemetry.current'))
+
+# Request info about particular file - should be None
+chosen_file = tmtc.send(GetFileInfo('/', 'telemetry.current'))
+pprint.pprint(chosen_file)
