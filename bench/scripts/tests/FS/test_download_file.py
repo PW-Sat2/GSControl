47a574784244373a72228c3fcb58863f3efd75e3
begreeen <michal.gumiela@gmail.com>
2017-11-02 19:34:44 +0100

    WIP: download and save file

--- bench/scripts/tests/FS/test_download_file.py
+++ bench/scripts/tests/FS/test_download_file.py
@@ -5,6 +5,7 @@ import tmtc
 from tm import TM, Check
 from tmtc import Tmtc
 from tc.fs import DownloadFile, GetFileInfo
+from tools.remote_files import RemoteFileTools
 
 tmtc = Tmtc()
 checker = Check(tmtc)
@@ -15,4 +16,8 @@ time.sleep(1)
 chosen_file = tmtc.send(GetFileInfo('/', 'telemetry.current'))
 pprint.pprint(chosen_file)
 
+# Download file
 chunks = tmtc.send(DownloadFile(chosen_file["File"], chosen_file["Chunks"]))
+
+# Save file
+RemoteFileTools.save_chunks('downloaded.raw', chunks)
