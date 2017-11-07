d4edd5c16bc4ffd09c84bfb5b4f0ea69a1ce10b0
begreeen <michal.gumiela@gmail.com>
2017-11-07 17:58:14 +0100

    WIP

---
+++ bench/scripts/tests/radfet_thermal_calibration.py
@@ -0,0 +1,21 @@
+from bench_init import *
+
+import time
+from tools.tools import RandomString
+
+
+@make_test
+def test_radfet_calibration():
+    # Check if OBC Terminal is available
+    print(obc.ping())
+
+    # Be sure that no experiment is currently running
+    tm.assert_equal(tm.Experiments.CurrentExperimentCode, 'None')
+
+    print(obc.payload_radfet_on())
+    time.sleep(10)
+
+    for i in range(10):
+        print(obc.payload_radfet_read())
+
+    print(obc.payload_radfet_off())
