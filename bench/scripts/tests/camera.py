679fe9b54add67fd4dacd3402addd2c984df4f87
begreeen <michal.gumiela@gmail.com>
2017-11-09 00:30:22 +0100

    Complete photo tc veryfication

--- bench/scripts/tests/camera.py
+++ bench/scripts/tests/camera.py
@@ -2,7 +2,16 @@ from bench_init import *
 import time
 
 @make_test
-def test_camera_telecommand(filename):
-    # Request photo
-    send(tc.camera.TakePhoto(tc.camera.CameraLocation.Wing, tc.camera.PhotoResolution.p128, 1, tc.camera.timedelta(0), filename))
-    print(scripts.photo.wait_for_photo(filename + "_0", 200))
+def test_camera_telecommand(filename_base):
+    photos_params = [{'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p128},
+                    {'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p240},
+                    {'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p480},
+                    {'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p128},
+                    {'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p240},
+                    {'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p480}]
+
+    # Request photos
+    for photo_params in photos_params:
+        filename = filename_base + '_' + photo_params['Camera'] + '_' + photo_params['Resolution']
+        send(tc.camera.TakePhoto(photo_params['Camera'], photo_params['Resolution'], 1, tc.camera.timedelta(0), filename))
+        print filename, " : ", scripts.photo.wait_for_photo(filename + "_0", 200)
