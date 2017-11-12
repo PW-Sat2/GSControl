7cfcffb876874b764b8cc6f6e1e1d92fb1d54f7d
begreeen <michal.gumiela@gmail.com>
2017-11-12 14:23:01 +0100

    More debug info

--- bench/scripts/tests/camera.py
+++ bench/scripts/tests/camera.py
@@ -14,7 +14,9 @@ def test_camera_telecommand(filename_base):
     for photo_params in photos_params:
         filename = filename_base + '_' + str(photo_params['Camera']) + '_' + str(photo_params['Resolution'])
         filename_obc = filename + "_0"
-        PrintLog(filename)
+        PrintLog("Sending telecommand to take photo " + filename)
         send(tc.camera.TakePhoto(photo_params['Camera'], photo_params['Resolution'], 1, tc.camera.timedelta(0), filename))
         if scripts.photo.wait_for_file(filename_obc, 200) is True:
             scripts.fs.download_photo(filename_obc)
+        else:
+            PrintLog("Photo " + filename + " failed!")
