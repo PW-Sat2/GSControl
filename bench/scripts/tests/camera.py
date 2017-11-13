26809824cf5d08c6f3db0a43e1f4a250c503c1a7
begreeen <michal.gumiela@gmail.com>
2017-11-13 13:26:14 +0100

    List of dicts describing photos to be performed

--- bench/scripts/tests/camera.py
+++ bench/scripts/tests/camera.py
@@ -1,17 +1,18 @@
 from bench_init import *
 import time
 
-@make_test
-def test_camera_telecommand(filename_base):
-    photos_params = [{'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p128},
+test_scenario_wing = [{'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p128},
                     {'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p240},
-                    {'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p480},
-                    {'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p128},
+                    {'Camera' : tc.camera.CameraLocation.Wing, 'Resolution' : tc.camera.PhotoResolution.p480}]
+
+test_scenario_nadir = [{'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p128},
                     {'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p240},
                     {'Camera' : tc.camera.CameraLocation.Nadir, 'Resolution' : tc.camera.PhotoResolution.p480}]
 
+@make_test
+def test_camera_telecommand(filename_base, test_scenario):
     # Request photos
-    for photo_params in photos_params:
+    for photo_params in test_scenario:
         filename = filename_base + '_' + str(photo_params['Camera']) + '_' + str(photo_params['Resolution'])
         filename_obc = filename + "_0"
         PrintLog("Sending telecommand to take photo " + filename)
