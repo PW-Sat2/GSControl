fb7dda2250e4c09febc0551e9ed9908e8c6cd526
Grzegorz Gajoch <gajoch@gmail.com>
2017-11-13 01:07:22 +0000

    fail camera test if photo wasn't found on OBC

--- bench/scripts/tests/camera.py
+++ bench/scripts/tests/camera.py
@@ -16,7 +16,11 @@ def test_camera_telecommand(filename_base):
         filename_obc = filename + "_0"
         PrintLog("Sending telecommand to take photo " + filename)
         send(tc.camera.TakePhoto(photo_params['Camera'], photo_params['Resolution'], 1, tc.camera.timedelta(0), filename))
+        failed = None
         if scripts.photo.wait_for_file(filename_obc, 200) is True:
             scripts.fs.download_photo(filename_obc)
         else:
             PrintLog("Photo " + filename + " failed!")
+            failed = filename
+
+        assert(failed is None)
