c6cbc79d4c04ea85ad863292de8cab75b0333922
begreeen <michal.gumiela@gmail.com>
2017-11-13 19:27:49 +0100

    Wait for file is in fs package

--- bench/scripts/tests/camera.py
+++ bench/scripts/tests/camera.py
@@ -18,7 +18,7 @@ def test_camera_telecommand(filename_base, test_scenario):
         PrintLog("Sending telecommand to take photo " + filename)
         send(tc.camera.TakePhoto(photo_params['Camera'], photo_params['Resolution'], 1, tc.camera.timedelta(0), filename))
         failed = None
-        if scripts.photo.wait_for_file(filename_obc, 200) is True:
+        if scripts.fs.wait_for_file(filename_obc, 200) is True:
             scripts.fs.download_photo(filename_obc)
         else:
             PrintLog("Photo " + filename + " failed!")
