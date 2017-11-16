09a762d409dc928041b6682ccce1074d5ec33df0
begreeen <michal.gumiela@gmail.com>
2017-11-16 17:07:39 +0100

    name fix

--- bench/tmtc/tc/eps.py
+++ bench/tmtc/tc/eps.py
@@ -1,9 +1,9 @@
 import telecommand.eps
-import response_frames.disable_overheat_submodule
+import response_frames.disable_overheat_submode
 
 class DisableOverheatSubmode(object):
     def __init__(self, controller):
     	self.controller = controller
 
     def send(self, tmtc):
-        return tmtc.send_tc_with_response(telecommand.eps.DisableOverheatSubmode, response_frames.disable_overheat_submodule.DisableOverheatSubmodeSuccessFrame, self.controller)
+        return tmtc.send_tc_with_response(telecommand.eps.DisableOverheatSubmode, response_frames.disable_overheat_submode.DisableOverheatSubmodeSuccessFrame, self.controller)
