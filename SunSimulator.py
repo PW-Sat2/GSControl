9e107e22c874ec2adf7f9db7311aa1d1643bf56d
Grzegorz Gajoch <gajoch@gmail.com>
2017-12-11 19:50:05 +0100

    Sail mode in sun simulator

--- SunSimulator.py
+++ SunSimulator.py
@@ -4,6 +4,6 @@ sys.path.insert(0, 'flatsat')
 from Flatsat import SolarPanelsSimulator
 
 simulator = SolarPanelsSimulator()
-simulator.random_tumbling(max_rotation_speed = 5, solar_panels_are_deployed = False)
+simulator.random_tumbling(max_rotation_speed = 5, solar_panels_are_deployed = False, sail_is_deployed = False)
 
 time.sleep(10)
