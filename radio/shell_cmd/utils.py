9b055e556ae70d149b845cb327d47a43e28f6911
Marcin Drobik <mdrobik@kplabs.pl>
2018-11-06 11:14:42 +0100

    Implemented load_downlink_frames_file method

--- radio/shell_cmd/utils.py
+++ radio/shell_cmd/utils.py
@@ -1,3 +1,6 @@
+import csv
+import base64
+
 def build(sender, rcv, frame_decoder, *args):
     def send_and_parse_beacon():
         """
@@ -12,6 +15,26 @@ def build(sender, rcv, frame_decoder, *args):
 
         return ParseBeacon.parse(beacon)
 
+    def load_downlink_frames_file(file_name):
+        """
+        Loads frames CSV file and parse downlink frames.
+        """
+        parsed_frames = []
+
+        with open(file_name, 'r') as csv_file:
+            frames_reader = csv.reader(csv_file, delimiter=',')
+            for row in frames_reader:
+                if row[1] != 'D':
+                    continue
+
+                binary_frame = base64.b64decode(row[2])
+                decoded_frame = frame_decoder.decode(rcv.decode_kiss(binary_frame))
+                parsed_frames.append(decoded_frame)
+
+        return parsed_frames
+
+
     return {
-        'send_and_parse_beacon': send_and_parse_beacon
+        'send_and_parse_beacon': send_and_parse_beacon,
+        'load_downlink_frames_file': load_downlink_frames_file
     }
