84be72eea016521868ad0dd8af2f287a5b4577d0
Piotr Kuligowski <xsaperx@gmail.com>
2018-11-17 13:51:41 +0100

    raw i2c power cycle for ctrl A added

--- radio/panic/power_cycle.py
+++ radio/panic/power_cycle.py
@@ -5,19 +5,19 @@ tasks = [
     [2, Sleep, WaitMode.NoWait],
     [tc.PowerCycleTelecommand(3), Send, WaitMode.NoWait],
     [2, Sleep, WaitMode.NoWait],
-    [tc.PowerCycleTelecommand(4), Send, WaitMode.NoWait],
+    [tc.RawI2C(13, 0, 0x35, 1, [0xE0]), Send, WaitMode.NoWait], # EPS A power cycle
     [2, Sleep, WaitMode.NoWait],
-    [tc.PowerCycleTelecommand(5), Send, WaitMode.NoWait],
+    [tc.RawI2C(13, 0, 0x35, 1, [0xE0]), Send, WaitMode.NoWait], # EPS A power cycle
     [2, Sleep, WaitMode.NoWait],
-    [tc.PowerCycleTelecommand(6), Send, WaitMode.NoWait],
+    [tc.RawI2C(13, 0, 0x35, 1, [0xE0]), Send, WaitMode.NoWait], # EPS A power cycle
     [2, Sleep, WaitMode.NoWait],
     [tc.PowerCycleTelecommand(7), Send, WaitMode.NoWait],
     [2, Sleep, WaitMode.NoWait],
-    [tc.PowerCycleTelecommand(8), Send, WaitMode.NoWait],
+    [tc.RawI2C(13, 0, 0x35, 1, [0xE0]), Send, WaitMode.NoWait], # EPS A power cycle
     [2, Sleep, WaitMode.NoWait],
     [tc.PowerCycleTelecommand(9), Send, WaitMode.NoWait],
     [2, Sleep, WaitMode.NoWait],
-    [tc.PowerCycleTelecommand(10), Send, WaitMode.NoWait],
+    [tc.RawI2C(13, 0, 0x35, 1, [0xE0]), Send, WaitMode.NoWait], # EPS A power cycle
     [20, Sleep, WaitMode.NoWait],
     [[tc.SendBeacon(), 15], SendLoop, WaitMode.NoWait],
 ]
