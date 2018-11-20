409c579e891f70419c38c36862f64bb39d135474
begreeen <michal.gumiela@gmail.com>
2018-11-21 00:41:05 +0100

    remove default uplink callsign

--- radio/radio_sender.py
+++ radio/radio_sender.py
@@ -25,7 +25,7 @@ class Wrap:
 
 
 class Sender:
-    def __init__(self, target="localhost", port=7000, source_callsign='SQ5PUI', destination_callsign='PWSAT2-0'):
+    def __init__(self, target="localhost", port=7000, source_callsign='', destination_callsign='PWSAT2-0'):
         self.context = zmq.Context.instance()
         self.sock = self.context.socket(zmq.PUB)
         self.sock.connect("tcp://%s:%d" % (target, port))
