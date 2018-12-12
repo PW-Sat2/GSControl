3031f37a964e5ea08d7c67fad094869328dd249a
Tomasz Martyniak <tom4r@userrs.noreply.github.com>
2018-12-12 07:28:33 +0100

    Load GRC env only when necessary

--- scripts/gnuradio.sh
+++ scripts/gnuradio.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "$SELF_DIR/_common.sh"
+source ${PWSAT_GS_GNURADIO}/setup_env.sh
 
 GR="${PWSAT_GS_GNURADIO}"
 GRC="${GSCONTROL}/gnuradio"
