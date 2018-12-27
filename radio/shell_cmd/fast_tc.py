cafadd01944b69b146c388391602a7f6ac2ac58f
Piotr Kuligowski <xsaperx@gmail.com>
2018-12-28 00:55:21 +0100

    fast setbitrate and sail

--- radio/shell_cmd/fast_tc.py
+++ radio/shell_cmd/fast_tc.py
@@ -1,5 +1,7 @@
 import telecommand as tc
 from radio.task_actions import WaitMode
+from devices.comm import BaudRate
+import sys
 
 
 def build(sender, rcv, frame_decoder, analyzer, ns):
@@ -14,7 +16,52 @@ def build(sender, rcv, frame_decoder, analyzer, ns):
         files.cid += 1
 
     files.cid = 200
+    
+    def bitrate_1200():
+        if bitrate_1200.cid == 200:
+            bitrate_1200.cid = 175
+        
+        ns['run']([
+            [tc.SetBitrate(bitrate_1200.cid, BaudRate.BaudRate1200), ns['Send'], WaitMode.Wait]
+        ])
+        
+        bitrate_1200.cid += 1
+    
+    bitrate_1200.cid = 175
+
+    def bitrate_9600():
+        if bitrate_9600.cid == 175:
+            bitrate_9600.cid = 150
+
+        ns['run']([
+            [tc.SetBitrate(bitrate_9600.cid, BaudRate.BaudRate9600), ns['Send'], WaitMode.Wait]
+        ])
+
+        bitrate_9600.cid += 1
+
+    bitrate_9600.cid = 150
+
+    def sail():
+        print('Are you sure to perform Sail Experiment? Type SAIL (uppercase) to confirm: ')
+        user = sys.stdin.readline()
+
+        if user.replace('\n', '').replace('\r', '') == 'SAIL':
+            if sail.cid == 125:
+                sail.cid = 100
+
+            ns['run']([
+                [tc.PerformSailExperiment(sail.cid), ns['Send'], WaitMode.Wait]
+            ])
+
+            sail.cid += 1
+        else:
+            print("Wrong answer. No Sail Experiment performed.")
+
+    sail.cid = 100
 
     return {
-        'files': files
+        'files': files,
+        'bitrate_1200': bitrate_1200,
+        'bitrate_9600': bitrate_9600,
+        'sail': sail
     }
