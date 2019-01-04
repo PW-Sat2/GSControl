8188c559e20dcd9036e9f841bae863e922add2a6
Grzegorz Gajoch <gajoch@gmail.com>
2019-01-04 23:37:33 +0100

    potential fix for pushing problems

--- auto_session/auto_start_session.py
+++ auto_session/auto_start_session.py
@@ -236,7 +236,17 @@ def stop_session():
     send_to_slack(gs_name + ': stopping session!')
     run_cmd('yes \'n\' | ' + gscontrol + '/scripts/stop_session.sh',
             'stop')
-    origin.push()
+    
+    origin.pull()
+    info = origin.push()[0]
+    print('info.ref', info.ref)  # Symbolic Reference or RemoteReference to the changed remote head or FETCH_HEAD
+    print('info.flags', info.flags)  # additional flags to be & with enumeration members,
+    # i.e. info.flags & info.REJECTED
+    # is 0 if ref is SymbolicReference
+    print('info.note', info.note)  # additional notes given by git-fetch intended for the user
+    print('info.old_commit', info.old_commit)  # if info.flags & info.FORCED_UPDATE|info.FAST_FORWARD,
+    # field is set to the previous location of ref, otherwise None
+    print('info.remote_ref_path', info.remote_ref_path)  # The path from which we fetched on the remote. It's the remote's version of our info.ref
 
 
 def all_frames_summary():
