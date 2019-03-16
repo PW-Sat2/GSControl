79acd8d165c62fc71eedc994e2498ba259127fc0
Grzegorz Gajoch <gajoch@gmail.com>
2019-03-16 04:46:43 +0000

    CR fixes

--- tools/upload_software/generate_tasklist.py
+++ tools/upload_software/generate_tasklist.py
@@ -2,6 +2,9 @@ import os
 import math
 import sys
 import argparse
+import datetime
+import md5
+import copy
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../PWSat2OBC/integration_tests'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
@@ -29,7 +32,7 @@ def parse_args():
 def process_boot_slots(boot_slots):
     boot_slots = [int(i) for i in boot_slots]
     for i in boot_slots:
-        assert 1 <= i <= 6, "Incorrect boot slot"
+        assert 0 <= i <= 5, "Incorrect boot slot"
     if boot_slots != [3, 4, 5]:
         print "================ WARNING =============="
         print "===   Boot slots not on fallback!   ==="
@@ -41,7 +44,8 @@ def process_boot_slots(boot_slots):
 args = parse_args()
 PART_SIZE = telecommand.WriteProgramPart.MAX_PART_SIZE
 
-binary_data = open(args.file, 'rb').read()
+binary_data_org = open(args.file, 'rb').read()
+binary_data = copy.copy(binary_data_org)
 binary_data = list(pad(binary_data, multiply_of=128, pad_with=0x1A))
 crc = calc_crc(binary_data)
 boot_slots = process_boot_slots(args.boot_slots)
@@ -58,18 +62,11 @@ print "  - CRC: {:4X}".format(crc)
 output_file = open(args.output, 'w')
 output_file.write('tasks = [\n')
 
-offset = 0
-while offset < len(binary_data):
-    binary = binary_data[offset:offset + PART_SIZE]
-    output_file.write("[tc.WriteProgramPart({}, {}, {}), Send, WaitMode.Wait],\n".format(
-                      str(boot_slots),
-                      str(offset),
-                      str('[{}]'.format(', '.join(hex(x) for x in binary)))))
-
-    offset += PART_SIZE
-
-output_file.write(']\n')
-output_file.close()
+output_file.write("    # Generated for file {} at {date:%Y-%m-%d %H:%M:%S}, md5sum: {md5}, crc: 0x{crc:4X}\n\n"
+                  .format(args.file,
+                  date=datetime.datetime.now(),
+                  md5=md5.new(binary_data_org).hexdigest(),
+                  crc=crc))
 
 mask = 0
 not_mask = 0b111111
@@ -77,11 +74,25 @@ for i in boot_slots:
     mask |= (1 << i)
     not_mask &= ~(1 << i)
 
-print '\n\n' + 50*"="
-print "telecommands to perform upload:"
 for i in boot_slots:
-    print "  [tc.EraseBootTableEntry([{}]), Send, WaitMode.Wait],".format(i)
-print "  ~~ send binary ~~ "
-print "  [tc.SetBootSlots(76, {}, {}), Send, WaitMode.Wait],".format(bin(mask), bin(not_mask))
-print "  [tc.FinalizeProgramEntry([{}], {}, 0x{:4X}, \"name\")), Send, WaitMode.Wait],".format(
-    boot_slots, len(binary_data), crc)
+    output_file.write("    [tc.EraseBootTableEntry({}), Send, WaitMode.Wait],\n".format(i))
+
+output_file.write('\n')
+
+offset = 0
+while offset < len(binary_data):
+    binary = binary_data[offset:offset + PART_SIZE]
+    output_file.write("    [tc.WriteProgramPart({}, {}, {}), Send, WaitMode.Wait],\n".format(
+                      str(boot_slots),
+                      str(offset),
+                      str('[{}]'.format(', '.join(hex(x) for x in binary)))))
+
+    offset += PART_SIZE
+
+output_file.write('\n')
+output_file.write("    [tc.FinalizeProgramEntry({}, {}, 0x{:4X}, \"name\"), Send, WaitMode.Wait],\n".format(
+    boot_slots, len(binary_data), crc))
+output_file.write("    [tc.SetBootSlots(76, {}, {}), Send, WaitMode.Wait],\n".format(bin(mask), bin(not_mask)))
+
+output_file.write(']\n')
+output_file.close()
