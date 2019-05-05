28c152661b5d53ad0aa573e98022d289fb480a4f
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-05-05 20:52:48 +0200

    Colored output.

--- tools/receive_radio_pwsat_pl/main.py
+++ tools/receive_radio_pwsat_pl/main.py
@@ -5,6 +5,7 @@ import sys
 import time
 import traceback
 import datetime
+import colorama
 
 
 parser = argparse.ArgumentParser()
@@ -16,9 +17,11 @@ parser.add_argument('-o', '--origin', required=False,
 
 args = parser.parse_args()
 
+colorama.init()
 
 rcv = Receiver(target=args.target, origin=args.origin)
 rcv.open_ws()
+print("Listening on websocket...")
 
 counter = 0
 while True:
@@ -26,7 +29,14 @@ while True:
         raw = rcv.get()
         frame_string_view = make_frame_object(raw)
         counter += 1
-        print("{} {:3d} {}".format(datetime.datetime.now().time().strftime('%H:%M:%S'), counter, frame_string_view))
+
+        timestamp_str = '{}{}{}'.format(colorama.Fore.CYAN, datetime.datetime.now().time().strftime('%H:%M:%S'), colorama.Fore.RESET)
+        counter_str = '{}{:3d}{}'.format(colorama.Fore.BLACK + colorama.Style.BRIGHT, counter, colorama.Fore.RESET + colorama.Style.RESET_ALL)
+
+        print("{} {} {}".format(timestamp_str, counter_str, frame_string_view))
+    except KeyboardInterrupt:
+        print("Ctrl+C received. Ending...")
+        break
     except Exception:
         rcv.open_ws()
         time.sleep(0.1)
