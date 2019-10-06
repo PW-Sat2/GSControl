69a4f6592822f781d90e142dba214bb620ea46d5
Tomasz Martyniak <tom4r@users.noreply.github.com>
2019-10-06 17:17:08 +0200

    Gpredict relay - better logging.

--- tools/gpredict_relay.py
+++ tools/gpredict_relay.py
@@ -1,8 +1,35 @@
 import socket
 import time
 import multiprocessing
+import logging
+import argparse
+import colorlog
 
 frequency = multiprocessing.Value('i', 0)
+
+def setup_log(silent):
+    logging.Formatter.converter = time.gmtime
+    root_logger_std = logging.getLogger()
+    console_handler = colorlog.StreamHandler()
+
+    formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(asctime)-15s %(levelname)s: [%(name)s] %(message)s",
+        log_colors={
+            'DEBUG': 'blue',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        }
+    )
+
+    console_handler.setFormatter(formatter)
+    root_logger_std.addHandler(console_handler)
+
+    if silent:
+        root_logger_std.setLevel(logging.INFO)
+    else:
+        root_logger_std.setLevel(logging.DEBUG)
 
 
 def set_freq(freq):
@@ -11,6 +38,7 @@ def set_freq(freq):
 
 
 def gpredict():
+    log = logging.getLogger("GPredict")
     bind_to = ('', 4400)
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -20,10 +48,10 @@ def gpredict():
     time.sleep(0.5)  # TODO: Find better way to know if init is all done
 
     while True:
-        print "Waiting for connection on: %s:%d" % bind_to
+        log.debug("Waiting for connection on: %s:%d" % bind_to)
         sock, addr = server.accept()
 
-        print "Connected from: %s:%d" % (addr[0], addr[1])
+        log.info("Connected from: %s:%d" % (addr[0], addr[1]))
 
         cur_freq = 0
         while True:
@@ -34,7 +62,7 @@ def gpredict():
             if data.startswith('F'):
                 freq = int(data[1:].strip())
                 if cur_freq != freq:
-                    print "New frequency: %d" % freq
+                    log.debug("New frequency: %d" % freq)
 
                     set_freq(freq)
 
@@ -44,11 +72,11 @@ def gpredict():
                 sock.sendall("f: %d\n" % cur_freq)
 
         sock.close()
-        print "Disconnected from: %s:%d" % (addr[0], addr[1])
-
+        log.info("Disconnected from: %s:%d" % (addr[0], addr[1]))
 
 
 def gnuradio():
+    log = logging.getLogger("Gnuradio")
     gnuradio_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     while True:
@@ -56,11 +84,11 @@ def gnuradio():
             gnuradio_socket.connect(('localhost', 4532))
             break
         except socket.error:
-            print "Wait for gnuradio"
+            log.debug("Wait for gnuradio")
             time.sleep(1)
 
     gnuradio_socket.settimeout(0.1)
-    print "Gnuradio Connected!"
+    log.info("Gnuradio Connected!")
     time.sleep(2)
 
     while 1:
@@ -81,32 +109,49 @@ def gnuradio():
 
             if frequency_now:
                 gnuradio_socket.send('F' + str(frequency_now) + '\n')
+        except KeyboardInterrupt:
+            log.info("Keyboard interrupt")
+            break           
         except Exception as e:
-            print e
+            log.exception("Error in processing", exc_info=e)
             break
 
-    print "Gnuradio disconnected!"
+    log.info("Gnuradio disconnected!")
     gnuradio_socket.shutdown(socket.SHUT_RDWR)
     gnuradio_socket.close()
 
 
-t = multiprocessing.Process(target=gpredict)
-t2 = multiprocessing.Process(target=gnuradio)
+def main(args):
+    t = multiprocessing.Process(target=gpredict)
+    t2 = multiprocessing.Process(target=gnuradio)
 
-while True:
-    try:
-        if not t.is_alive():
-            t = multiprocessing.Process(target=gpredict)
-            t.start()
-        if not t2.is_alive():
-            t2 = multiprocessing.Process(target=gnuradio)
-            t2.start()
-        time.sleep(1)
+    setup_log(args.silent)
+    log = logging.getLogger("Main")
 
-    except KeyboardInterrupt:
-        print "Ending!"
-        t.terminate()
-        t2.terminate()
-        exit(0)
+    while True:
+        try:
+            if not t.is_alive():
+                t = multiprocessing.Process(target=gpredict)
+                t.start()
+            if not t2.is_alive():
+                t2 = multiprocessing.Process(target=gnuradio)
+                t2.start()
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            log.info("Ending!")
+            t.terminate()
+            t2.terminate()
+            exit(0)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s', '--silent', action='store_true',
+                        help="Suppress most of output (service use)")
+
+    return parser.parse_args()
+
+if __name__ == '__main__': 
+    main(parse_args())
