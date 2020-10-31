2804bb81cbbfbbc8699abbad910ae45a23b3f819
Tomasz Martyniak <tom4r@users.noreply.github.com>
2020-10-31 13:09:38 +0100

    Added script for running.

---
+++ scripts/satnogs_uploader.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source "$SELF_DIR/_common.sh"
+
+python2.7 "${GSCONTROL}/tools/satnogs_beacon_uploader/run.py" -c "${GSCONTROL}/tools/satnogs_beacon_uploader/satnogs_config.py"
