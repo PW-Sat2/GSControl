3b826501569b4c569c4fc6ee6063ff66bc4b7cb3
Tomasz Martyniak <tom4r@userrs.noreply.github.com>
2020-11-01 18:38:52 +0100

    Do actual upload in script

--- scripts/satnogs_uploader.sh
+++ scripts/satnogs_uploader.sh
@@ -2,4 +2,4 @@
 SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "$SELF_DIR/_common.sh"
 
-python2.7 "${GSCONTROL}/tools/satnogs_beacon_uploader/run.py" -c "${GSCONTROL}/tools/satnogs_beacon_uploader/satnogs_config.py"
+python2.7 "${GSCONTROL}/tools/satnogs_beacon_uploader/run.py" -c "${GSCONTROL}/tools/satnogs_beacon_uploader/satnogs_config.py" -u
